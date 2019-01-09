de5c3f058043766cffab5a080154a9c61828f971
shinyscyther <mesibovj@gmail.com>
2019-01-09 14:26:04 -0500

    began working on a function to perform an AJAX request and display reviews of books to the html

---
+++ assets/logic.js
@@ -0,0 +1,6 @@
+var Book1 = "Station+11+by+Emily+St+John+Mande";
+var Book2 = "Uprooted+by+Naomi+Novik";
+
+function displayReviews() {
+  var queryURL = "http://idreambooks.com/api/books/reviews.json?q={keywords}&key=36ecbc8d8618c9f56345cf3e322fa1355b25fc32"
+}
