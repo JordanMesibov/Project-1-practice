4095153f0cb6c66ec964325e495df4fc6afcc7f5
shinyscyther <mesibovj@gmail.com>
2019-01-09 16:59:17 -0500

    achieved a successful ajax call to the iDreamBooks API in order to console log a JSON response object of a book and its associated reviews

--- assets/logic.js
+++ assets/logic.js
@@ -1,6 +1,30 @@
-var Book1 = "Station+11+by+Emily+St+John+Mande";
-var Book2 = "Uprooted+by+Naomi+Novik";
-
-function displayReviews() {
-  var queryURL = "http://idreambooks.com/api/books/reviews.json?q={keywords}&key=36ecbc8d8618c9f56345cf3e322fa1355b25fc32"
+var Book1 = {
+  title : "Station+Eleven",
+  author : "Emily+St+John+Mandel"
 }
+console.log(Book1);
+
+var Book2 = {
+  title: "Uprooted",
+  author : "Naomi+Novik"
+}
+console.log(Book2);
+
+function displayBook1Review() {
+  var queryURL = "http://idreambooks.com/api/books/reviews.json?q=" + Book1.title + "&key=36ecbc8d8618c9f56345cf3e322fa1355b25fc32"
+  console.log(queryURL);
+  console.log(Book1.title);
+  console.log(Book1.author);
+  //make the ajax call to the iDreamBooks API
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log("The response to the ajax call is below");
+    console.log(response);
+  })
+
+
+}
+
+displayBook1Review(Book1);
